1888c5d35752c408525760dededab6b8e1a700d9
FARAJ AHMED AHMED AL-QDWRY <faraj@siswa365.um.edu.my>
2019-04-16 14:12:48 +0800

    first commint

---
+++ app/src/main/java/com/aqsoft/newapp/MainActivity.java
@@ -0,0 +1,68 @@
+package com.aqsoft.newapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button bt1;
+    TextView tex1;
+    int count = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        bt1 = findViewById(R.id.button1);
+        tex1 = findViewById(R.id.temp);
+
+        bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               // update view
+                count++;
+                tex1.setText(count+" ");
+            }
+        });
+
+
+        Toast.makeText(this, "onCreate",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this, "onStart",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, "onResume",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this, "onPause",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this, "onStop",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "onDestroy",Toast.LENGTH_SHORT).show();
+    }
+
+}
